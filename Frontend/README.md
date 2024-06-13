819455a5afe34605169cf90316424344668f0936
Mafe Gamboa <158193517+Mafegam@users.noreply.github.com>
2024-06-13 11:46:24 -0500

    Proyecto inicializado

---
+++ Frontend/README.md
@@ -0,0 +1,14 @@
+# LearnDev
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.7.
+
+## Development server
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+
+## Steps taken to build
+1. Create Github repository and sincronize with local
+2. Start Angular project with ng new Learn-Dev
+3. Clear Angular's default HTML 
+4. Create components withg ng generate component Component/Name --skip-tests
+5. Install libraries 
---
+++ Frontend/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { CardComponent } from './Components/card/card.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, SidebarComponent, CardComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Learn_Dev';
+}
