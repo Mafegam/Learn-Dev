bfe6e06149fed38c97836913534403201143aa6d
Mafe Gamboa <158193517+Mafegam@users.noreply.github.com>
2024-08-01 22:09:43 -0500

    Thirsday EOD

--- Frontend/src/app/Components/home/navbar/navbar.component.ts
+++ Frontend/src/app/Components/home/navbar/navbar.component.ts
@@ -4,10 +4,14 @@ import { RouterLink, Router } from '@angular/router';
 import { LoginService } from '../../../Services/login.service';
 import { WatchLaterService } from '../../../Services/watch-later.service';
 
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatIconModule } from '@angular/material/icon';
+
+
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatBadgeModule, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
@@ -16,11 +20,14 @@ export class NavbarComponent {
   watchLaterService = inject(WatchLaterService);
 
   retrievedList: any[] = [];
+  numberOfItems: number = 0
 
   retrieveList() {
     this.watchLaterService.retrieveList().subscribe((respuesta: any) => {
       if (respuesta) {
         this.retrievedList = respuesta.datos
+        this.numberOfItems = this.retrievedList.length
+        this.ngOnInit();
       } else {
         console.log(Error);
       }
@@ -28,7 +35,7 @@ export class NavbarComponent {
   }
 
   removeTutorial(tutorialID: string) {
-    
+
     this.watchLaterService.removeTutorial(tutorialID).subscribe((respuesta: any) => {
       if (respuesta) {
         this.ngOnInit();
@@ -38,7 +45,7 @@ export class NavbarComponent {
     })
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.retrieveList();
   }
 
--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -7,11 +7,12 @@ import { provideAnimations } from "@angular/platform-browser/animations";
 import { provideToastr } from 'ngx-toastr';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
     provideAnimations(),
-    provideToastr()]
+    provideToastr(), provideAnimationsAsync()]
 };
